d3cb8f58d47352f9df50b410d243e13021f517c3
Jesus Roman <jesus_fime@outlook.es>
2023-08-24 12:32:10 -0600

    Agregando Hooks y funcion de HandleSubmit con el error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,8 +3,29 @@ import { useState, useEffect } from 'react'
 
 const Formulario = () => {
     //Creando nuestro primer state
+    const [nombre, setNombre] = useState('');
+    const [propietario, setPropietario] = useState('');
+    const [email, setEmail] = useState('');
+    const [fecha, setFecha] = useState('');
+    const [sintomas, setSintomas] = useState('');
+
+    //Generando un state mas para el error
+    const [error, setError] = useState(false);
+
+    const handleSubmit = (e) =>{ //tambien le podemos pasar el evento
+        e.preventDefault();
+        //Validacion del Formulario
+        if([nombre, propietario, email, fecha, sintomas].includes('')){ //generamos un arreglo con todas las variables para despues poder hacer uso de includes que este vacio
+            console.log('Hay vacio');
+            setError(true); //cambiamos el error a true
+            return;
+        }
+        
+        setError(false);
+    }
+
   return (
-    <div className='md:w-1/2 lg:w-2/5'>
+    <div className='md:w-1/2 lg:w-2/5 mx-5'>
         <h2 className='font-black text-3xl text-center'>
             Seguimiento Pacientes
         </h2>
@@ -14,7 +35,14 @@ const Formulario = () => {
             <span className='text-indigo-600 font-bold text-lg'>Administralos</span>
         </p>
 
-        <form className='bg-white shadow-md rounded-lg py-10 px-5 mb-10' action="">
+        <form className='bg-white shadow-md rounded-lg py-10 px-5 mb-10' action="" onSubmit={handleSubmit}>
+
+            {error && 
+                <div className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded'>
+                    <p>Todos los campos son obligatorios</p>
+                </div>
+            } {/* <-------COLOCANDO ERROR EN CASO DE QUE ERROR SEA TRUE, para eso sirve el && (si error entonces imprime en un div con un p si hay error) */}
+            
             <div className='mb-5'>
                 <label className='block text-gray-700 uppercase font-bold' htmlFor="mascota">Nombre Mascota</label>
 
@@ -23,6 +51,8 @@ const Formulario = () => {
                 type="text" 
                 placeholder='Nombre de la mascota'
                 className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                value={nombre}
+                onChange={(e)=>setNombre(e.target.value)} /* cada que cambie este evento, la funcion setNombre va a editar la variable nombre */
                 />
             </div>
 
@@ -34,6 +64,8 @@ const Formulario = () => {
                 type="text" 
                 placeholder='Nombre del propietario'
                 className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                value={propietario}
+                onChange={(e)=>setPropietario(e.target.value)}
                 />
             </div>
 
@@ -45,6 +77,8 @@ const Formulario = () => {
                 type="email" 
                 placeholder='Email contacto propietario'
                 className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
                 />
             </div>
 
@@ -55,6 +89,8 @@ const Formulario = () => {
                 id='alta'
                 type="date" 
                 className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                value={fecha}
+                onChange={(e)=>setFecha(e.target.value)}
                 />
             </div>
 
@@ -67,6 +103,8 @@ const Formulario = () => {
                 cols="30" rows="10" 
                 placeholder='Describe los sintomas'
                 className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                value={sintomas}
+                onChange={(e)=>setSintomas(e.target.value)}
                 >
                 </textarea>
             </div>
