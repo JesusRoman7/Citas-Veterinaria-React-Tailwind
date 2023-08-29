b0bb2883a90957e4fb503313e0d7d173915eee4c
Jesus Roman <jesus_fime@outlook.es>
2023-08-29 11:59:12 -0600

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 import ListadoPacientes from './components/ListadoPacientes'
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Paciente = ({paciente, setPaciente, eliminarPaciente}) => {
     console.log(paciente);
